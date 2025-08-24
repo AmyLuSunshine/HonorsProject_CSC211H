dfb40986f6746ad223485525aad11902fd13cb4f
Yangmei (Amy) Lu <109566558+AmyLuSunshine@users.noreply.github.com>
2025-08-24 16:41:09 -0400

    switched to community version from enterprise version

---
+++ build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_campus_Hire_app_FILE C:/Users/yangm/Documents/Masters/CSProjects/Qt_HonorsProject_CSC211H/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/campus_Hire_app.exe)
+set(__QT_DEPLOY_TARGET_campus_Hire_app_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_campus_Hire_app_RUNTIME_DLLS C:/Qt-Community/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt-Community/6.9.0/mingw_64/bin/Qt6Sql.dll;C:/Qt-Community/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt-Community/6.9.0/mingw_64/bin/Qt6Core.dll)
